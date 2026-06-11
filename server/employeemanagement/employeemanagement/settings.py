9e9ff11ff6317403574378c9ccbd0a8ebf052bab
Febi Varghese <febi.varghese@innovaturelabs.com>
2026-06-11 18:00:52 +0530

    update docker &  requirements

--- server/employeemanagement/employeemanagement/settings.py
+++ server/employeemanagement/employeemanagement/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee.apps.EmployeeConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
